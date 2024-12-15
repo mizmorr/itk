61fcc5e96b6195e07f3ddb0dac14aebc38af7213
mizmorr <mizmorrr@gmail.com>
2024-12-16 01:25:08 +0300

    upd: add lcg test

--- task6/task6_test.go
+++ task6/task6_test.go
@@ -1,7 +1,21 @@
 package task6
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestTask(t *testing.T) {
 	Task6()
+}
+
+func TestLCG(t *testing.T) {
+	lcg := newLCG(42, 1103515245, 12345, 1<<31)
+	generatedValues := make(map[int64]struct{})
+	for i := 0; i < 1000; i++ {
+		value := lcg.next()
+		if _, ok := generatedValues[value]; ok {
+			t.Error("Duplicate value generated")
+		}
+		generatedValues[value] = struct{}{}
+	}
 }
