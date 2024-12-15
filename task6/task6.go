1724f809c97b8e5b7c952889c7aa82574c570664
mizmorr <mizmorrr@gmail.com>
2024-12-16 01:17:45 +0300

    upd: add lcg

--- task6/task6.go
+++ task6/task6.go
@@ -8,9 +8,44 @@ import (
 
 const timeToStop = 10
 
+type lcg struct {
+	seed       int64
+	multiplier int64
+	increment  int64
+	modulus    int64
+}
+
+func newLCG(seed, multiplier, increment, modulus int64) *lcg {
+	return &lcg{
+		seed:       seed,
+		multiplier: multiplier,
+		increment:  increment,
+		modulus:    modulus,
+	}
+}
+
+func (lcg *lcg) next() int64 {
+	lcg.seed = (lcg.multiplier*lcg.seed + lcg.increment) % lcg.modulus
+	var additionalMultiplier int64 = 1
+	if rand.Intn(2) == 0 {
+		additionalMultiplier = -1
+	}
+	return lcg.seed * additionalMultiplier
+}
+
+func posix() *lcg {
+	return newLCG(
+		42,
+		1103515245,
+		12345,
+		1<<31,
+	)
+}
+
 func Task6() {
-	values := make(chan int)
+	values := make(chan int64)
 	done := make(chan struct{})
+	generator := posix()
 	go func() {
 		for {
 			select {
@@ -18,7 +53,7 @@ func Task6() {
 				close(values)
 				return
 			default:
-				values <- rand.Intn(1000 + 1)
+				values <- generator.next()
 			}
 		}
 	}()
