2a53d1af65affb690c87890606353a74dce256db
mizmorr <mizmorrr@gmail.com>
2024-12-15 23:44:45 +0300

    upd: benchmarks

--- task4/task4_test.go
+++ task4/task4_test.go
@@ -1,23 +1,32 @@
 package task4
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func BenchmarkGetEqualsMap(b *testing.B) {
-	first := []int{-2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
-	second := []int{-2, 4, 5, 6, 7, 8, 9, 10, 11, 15, 17, 19}
+func generateSlice(n int) []int {
+	resultSlice := make([]int, n)
+	for i := 0; i < n; i++ {
+		resultSlice[i] = rand.Intn(n * n)
+	}
+	return resultSlice
+}
 
+var (
+	first  = generateSlice(10000)
+	second = generateSlice(10000)
+)
+
+func BenchmarkGetEqualsMap(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		GetEqualsMap(first, second)
 	}
 }
 
 func BenchmarkGetEqualsSort(b *testing.B) {
-	first := []int{-2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}
-	second := []int{-2, 4, 5, 6, 7, 8, 9, 10, 11, 15, 17, 19}
 	for i := 0; i < b.N; i++ {
 		GetEqualsSort(first, second)
 	}
